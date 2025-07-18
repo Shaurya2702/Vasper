36134fd2ce999d4b0113ba6a90fe14f3d1887deb
Shaurya Sharma <shaurya270204@gmail.com>
2025-07-18 14:43:40 +0530

    Initial commit with source code and executable

---
+++ setWallpaper.ps1
@@ -0,0 +1,14 @@
+# SetWallpaper.ps1
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    public static void Set(string path) {
+        SystemParametersInfo(20, 0, path, 0x01 | 0x02);
+    }
+}
+'@
+
+[Wallpaper]::Set($args[0])
